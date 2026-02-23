5661b9b3596d63240d8b63dfa3f142b16f880176
Puneet Rao <151575618+RaoPuneet@users.noreply.github.com>
2026-02-23 10:23:03 +0530

    Update Django settings with new configuration

--- HandicraftProject/HandicraftProject/settings.py
+++ HandicraftProject/HandicraftProject/settings.py
@@ -126,4 +126,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'puneetrao2003@gmail.com'
-EMAIL_HOST_PASSWORD = 'jxpolpmywlyrunsd'
+EMAIL_HOST_PASSWORD = 'Use yours'
+
